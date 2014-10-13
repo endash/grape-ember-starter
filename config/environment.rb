9d7688189d4c08385d8865b82f929215412fa761
Christopher Swasey <christopher.swasey@gmail.com>
2014-10-13 18:41:04 -0400

    Remove some files we don't want right now, add api.rb now that it's actually been saved, and remove a few other problems

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,7 @@ ENV['RACK_ENV'] ||= 'test'
 
 require File.expand_path('../application', __FILE__)
 
-ActiveRecord::Base.configurations = YAML::load(File.open('./config/database.yml'))
-ActiveRecord::Base.establish_connection ENV['RACK_ENV']
+unless ENV['RACK_ENV'] == 'test'
+  ActiveRecord::Base.configurations = YAML::load(File.open('./config/database.yml'))
+  ActiveRecord::Base.establish_connection ENV['RACK_ENV']
+end
